6a263ee42b62b0237ad9703b1a62a5773ae28ef6
Max Chernyshov <chernyshovmaksim56@gmail.com>
2024-11-20 07:44:46 +0500

    add mmenu-light

--- src/main.js
+++ src/main.js
@@ -1,7 +1,26 @@
 import "./style.css";
+import "mmenu-light/dist/mmenu-light.css";
 
 import $ from "jquery";
+import "mmenu-light/dist/mmenu-light";
 
 $(document).ready((e) => {
 	console.log("Page is loadded");
+
+	const menu = new MmenuLight(
+		document.querySelector("#menu"),
+		"(max-width: 600px)"
+	);
+
+	const navigator = menu.navigation();
+	const drawer = menu.offcanvas({
+		position: "right",
+	});
+
+	document
+		.querySelector("a[href='#menu']")
+		.addEventListener("click", (evnt) => {
+			evnt.preventDefault();
+			drawer.open();
+		});
 });
