b6c0729992d129d7102db94a7e7703550d00b96a
chernyshovmaksim <chernyshovmaksim56@gmail.com>
2024-11-27 13:05:55 +0500

    Changes to be committed: 	new file:   vite.config.js

---
+++ vite.config.js
@@ -0,0 +1,17 @@
+// vite.config.js
+import { resolve } from "path";
+import { defineConfig } from "vite";
+
+export default defineConfig({
+	build: {
+		rollupOptions: {
+			input: {
+				main: resolve(__dirname, "index.html"),
+				catalog: resolve(__dirname, "catalog.html"),
+				contact: resolve(__dirname, "contact.html"),
+				favorites: resolve(__dirname, "favorites.html"),
+				product: resolve(__dirname, "product.html"),
+			},
+		},
+	},
+});
